12e4498ed64d6155f25012653240c2badda0ee71
bdhamithkumara <damithkumararoxx55@gmail.com>
2023-11-21 15:25:15 +0530

    change setting

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,11 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const metadata = {
+  title: 'Sanity + NextJS',
+  description: 'NextJS 14.0.2 Sanity 3.19.3 ',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
