18400de74c583a6730d57f788476dde416b0a19c
bdhamithkumara <damithkumararoxx55@gmail.com>
2023-11-22 00:35:43 +0530

    ww

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -123,7 +123,7 @@ const SinglePage = () => {
         <meta name="keywords" content="Next.js, React, SEO" />
         <meta property="og:title" content="My Page Title for Open Graph" />
         <meta property="og:description" content="This is a page description for Open Graph." />
-        <meta property="og:image" content={avatar}/>
+        <meta property="og:image" content="https://i.postimg.cc/90Ytr3B7/8.jpg"/>
         <meta name="application-name" content="Next.js" />
         <meta name="author" content="Seb" />
         <link rel="author" href="https://nextjs.org" />
@@ -143,9 +143,9 @@ const SinglePage = () => {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="sample title by damith" />
         <meta property="og:description" content="sample description by damith" />
-        <meta property="og:image" content={avatar} />
+        <meta property="og:image" content="https://i.postimg.cc/90Ytr3B7/8.jpg" />
 
-        <meta name="twitter:card" content={avatar}/>
+        <meta name="twitter:card" content="https://i.postimg.cc/90Ytr3B7/8.jpg"/>
         <meta name="twitter:image:alt" content="PSGC API Banner"/>
         <meta name="twitter:title" content="Philippine Standard Geographic Code API"/>
         <meta name="twitter:description" content="API used for listing all the region, province, city, municipality, barangay, and its data. Philippine Standard Geographic Codes (PSGC) data came from Philippine Statistics Authority."/>
