b2e11605c6faa99793ad1365755488119fec564d
bdhamithkumara <damithkumararoxx55@gmail.com>
2023-11-22 17:30:36 +0530

    ok fineee

--- app/blog/[slug]/MyPage.js
+++ app/blog/[slug]/MyPage.js
@@ -18,8 +18,14 @@ export async function getStaticProps() {
         <Head>
           <title>{pageTitle}</title>
           <meta name="description" content={pageDescription} />
+          <meta property="og:title" content="Images in Next.js"/>
+          <meta property="og:description" content="A guide on how to optimize "/>
+          <meta property="og:image" content="https://sanity-and-next-js-14.vercel.app/og?title=hiThisIsMyBlog"/>
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta name="twitter:title" content="Generate Dynamic Open Graph"/>
+          <meta name="twitter:description" content="A guide on how to optimize "/>
+          <meta name="twitter:image" content="https://sanity-and-next-js-14.vercel.app/og?title=mytesttt"></meta>
         </Head>
-        {/* page content */}
       </>
     );
   }
